f6f82e4be17d5eaf8ba503c825484c8c899c2d5e
Yefri Gonzalez <yefrig00@gmail.com>
2023-01-12 18:24:11 -0600

    Terminando diseño y validaciones de Formulario para guardar usuarios

--- src/app/modules/user/pages/user-page/user-page.component.ts
+++ src/app/modules/user/pages/user-page/user-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -13,23 +14,25 @@ export interface UserData {
   codigo_usuario:string;
 }
 
-
-
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
   styleUrls: ['./user-page.component.scss']
 })
 export class UserPageComponent {
+  hide=true;
+  public passwordType = 'password';
+  public loading = false;
+
+  registerForm!:FormGroup;
   displayedColumns: string[] = ['id','DPI', 'nombres', 'apellidos', 'username','email','codigo_usuario','options'];
   dataSource: MatTableDataSource<UserData>;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private userServide:UserService) {
-    // Create 100 users
-    // Assign the data to the data source for the table to render
+  constructor(private userServide:UserService,private formBuilder: FormBuilder) {
     this.dataSource = new MatTableDataSource();
+    this.buildForm();
   }
   ngOnInit() {
     this.userServide.getAllUsers().subscribe(data=>{
@@ -37,6 +40,44 @@ export class UserPageComponent {
     });
   }
 
+
+  private buildForm(){
+    this.registerForm=this.formBuilder.group({
+      DPI:['', [Validators.required,Validators.pattern(/^((\\+91-?)|0)?[0-9]{13}$/)]],
+      nombres:['', [Validators.required]],
+      apellidos:['', [Validators.required]],
+      email:['', [Validators.required,Validators.email]],
+      codigo_usuario:['', [Validators.required]],
+      password:['', [Validators.required,Validators.pattern(/^(?=\D*\d)(?=[^a-z]*[a-z])(?=.*[$@$!%*?&])(?=[^A-Z]*[A-Z]).{8,30}$/)]],
+      passwordConfirm:['',[Validators.required,Validators.pattern(/^(?=\D*\d)(?=[^a-z]*[a-z])(?=.*[$@$!%*?&])(?=[^A-Z]*[A-Z]).{8,30}$/),PasswordValidation.MatchPassword]],
+      username:['', [Validators.required]]
+    });
+  }
+  get DPI() {
+    return this.registerForm.get('DPI');
+  }
+
+  get Nombres() {
+    return this.registerForm.get('nombres');
+  }
+  get Email() {
+    return this.registerForm.get('email');
+  }
+  get CodigoUsuario() {
+    return this.registerForm.get('codigo_usuario');
+  }
+  get Password() {
+    return this.registerForm.get('password');
+  }
+  get PasswordConfirm() {
+    return this.registerForm.get('passwordConfirm');
+  }
+  get Username() {
+    return this.registerForm.get('username');
+  }
+  get Apellidos() {
+    return this.registerForm.get('apellidos');
+  }
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
@@ -50,21 +91,32 @@ export class UserPageComponent {
       this.dataSource.paginator.firstPage();
     }
   }
+
+  registerUser(){
+    
+  }
 }
 
-/** Builds and returns a new User. 
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-    '.';
+export class PasswordValidation {
 
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
-  };
+  static MatchPassword(AC: AbstractControl) {
+     const formGroup = AC.parent;
+     if (formGroup) {
+          const passwordControl = formGroup.get('password'); // to get value in input tag
+          const confirmPasswordControl = formGroup.get('passwordConfirm'); // to get value in input tag
 
-}*/
+          if (passwordControl && confirmPasswordControl) {
+              const password = passwordControl.value;
+              const confirmPassword = confirmPasswordControl.value;
+              if (password !== confirmPassword) { 
+                  return { matchPassword: true };
+              } else {
+                  return null;
+              }
+          }
+     }
+
+     return null;
+  }
+}
+
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -5,6 +5,7 @@ import { UserRoutingModule } from './user-routing.module';
 import { HeaderComponent } from '@components/header';
 import { AppMaterialModule } from 'src/app/app-material/app-material.module';
 import { UserPageComponent } from './pages/user-page';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UserPageComponent],
@@ -12,7 +13,8 @@ import { UserPageComponent } from './pages/user-page';
     CommonModule,
     UserRoutingModule,
     HeaderComponent,
-    AppMaterialModule
+    AppMaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
