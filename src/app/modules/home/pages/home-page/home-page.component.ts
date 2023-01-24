7b2889ca24ce8dd7882558f1ce32cb521442b4a0
Yefri Gonzalez <yefrig00@gmail.com>
2023-01-23 22:43:53 -0600

    Agregando botones tipo arbol para grupos

--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -1,7 +1,9 @@
 import { Component,OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { IPermission } from '@core/interfaces/i-permission';
 import { IProject, IProjectAssignment } from '@core/interfaces/i-project';
+import { IRole, IroleByUser } from '@core/interfaces/i-role';
 import { AuthService } from '@modules/auth';
 
 @Component({
@@ -11,20 +13,34 @@ import { AuthService } from '@modules/auth';
 })
 export class HomePageComponent {
   //public projects: any;
+  projectList:IProject[]=[];
+  roles:IroleByUser[]=[];
   public projects: IProjectAssignment[] = [];
-  public permisos:IPermission[]=[]
+  public permisos:IPermission[][]=[]
+  displayedColumns: string[] = ['proyecto','options'];
+  dataSource: MatTableDataSource<IProject>;
   id:number=0;
   constructor(private auth: AuthService, private router: Router) {
-    this.id=Number(localStorage.getItem('id'));
-    this.getProject();
+    this.dataSource=new MatTableDataSource();
+    
   }
 
+  ngOnInit(){
+    this.id=Number(localStorage.getItem('id'));
+    this.getProject();
+    console.log(this.projectList)
+  }
   getProject(){
     this.auth.getProjects(this.id).subscribe((data)=>{
       this.projects=data
-      this.projects.forEach((data)=>{
-        this.permisos=data.permisos
+      this.projects.forEach((dat)=>{
+        this.projectList.push(dat.proyecto[0]);
+        dat.roles.forEach(rol=>{
+          this.roles.push(rol);
+        })
+        
       })
+      this.dataSource=new MatTableDataSource(this.projectList);
     });
   }
 }
