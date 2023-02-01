d8774c6d4baa6b054c6c5d885acb1b1c8815dc18
Lucia Vasquez <bvlucia34@gmail.com>
2023-02-01 12:43:56 -0600

    diseño de botones para poder subir y descargar archivos

--- src/app/modules/project-home/pages/home-project-page/home-project-page.component.ts
+++ src/app/modules/project-home/pages/home-project-page/home-project-page.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Params, Route } from '@angular/router';
+import { IRole } from '@core/interfaces/i-role';
 
 @Component({
   selector: 'app-home-project-page',
   templateUrl: './home-project-page.component.html',
   styleUrls: ['./home-project-page.component.scss']
 })
+
 export class HomeProjectPageComponent {
   project!:string;
+  roles: IRole[] = [];
+  
   constructor(private routes:ActivatedRoute){
   }
 
