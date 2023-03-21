5e3456d34d919df224c362b6f19eb088aa00bcef
Lucía Vásquez <bvlucia34@gmail.com>
2023-03-20 19:12:25 -0600

    Implementando spinner con porcentaje en la parte del dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app-material/app-material.module';
-import {  ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { SpinnerModule } from '@core/spinner/spinner.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SpinnerInterceptor } from '@core/interceptors/spinner.interceptor';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 import { HttpErrorInterceptor } from '@core/interceptors/HttpErrorInterceptor.interceptor';
 import { NgxPermissionsModule } from 'ngx-permissions';
-import {MatTabsModule} from '@angular/material/tabs';
-import {DragDropModule} from '@angular/cdk/drag-drop';
+import { MatTabsModule } from '@angular/material/tabs';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { TokenInterceptor } from '@core/interceptors/token.interceptor';
-import {MatTreeModule} from '@angular/material/tree';
+import { MatTreeModule } from '@angular/material/tree';
 import { DrawerRailModule } from 'angular-material-rail-drawer';
 
 @NgModule({
@@ -38,13 +38,13 @@ import { DrawerRailModule } from 'angular-material-rail-drawer';
     DragDropModule,
     MatTreeModule,
     DrawerRailModule,
-    NgxPermissionsModule.forRoot()
+    NgxPermissionsModule.forRoot(),
   ],
 
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true},
-    {provide:HTTP_INTERCEPTORS,useClass:TokenInterceptor,multi:true},
-    {provide:HTTP_INTERCEPTORS,useClass:HttpErrorInterceptor,multi:true}, 
+    { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
 })
