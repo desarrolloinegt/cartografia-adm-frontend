27b14310420ac8f6103e8d645300c893691d40cf
Yefri Gonzalez <yefrig00@gmail.com>
2023-03-20 23:12:57 -0600

    Spinner de progreso terminado

--- src/app/modules/project-home/components/control-dashboard/control-dashboard.component.ts
+++ src/app/modules/project-home/components/control-dashboard/control-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ControlDashboardService } from '@modules/project-home/services/control-dashboard.service';
+import { ProjectHomeService } from '@modules/project-home/services/project-home.service';
 
 @Component({
   selector: 'app-control-dashboard',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./control-dashboard.component.scss']
 })
 export class ControlDashboardComponent {
-
+  total:number;
+  finished:number;
+  progress:number;
+  projectId:number;
+  percentajeFinished:number;
+  percentajeProgress:number;
+  constructor(private projectHomeService:ProjectHomeService, private dashService:ControlDashboardService){
+    this.total=0;
+    this.finished=0;
+    this.progress=0;
+    this.projectId=-1;
+    this.percentajeFinished=0;
+    this.percentajeProgress=0;
+  }
+  ngOnInit(){
+    let project=localStorage.getItem('project')||'';
+    this.projectHomeService.getIdProject(project).subscribe((data)=>{
+      this.projectId=data;
+      this.getData();
+    })
+  }
+  getData(){
+    let data={proyecto_id:this.projectId}
+    this.dashService.getData(data).subscribe((resp)=>{
+      console.log(resp)
+      this.finished=resp.finalizados;
+      this.total=resp.total;
+      this.progress=resp.progreso;
+      this.percentajeFinished=(resp.finalizados*100)/this.total;
+      this.percentajeProgress=(resp.progreso*100)/this.total;
+    })
+  }
+  styleFinished(){
+    return {"stroke-dasharray":`${this.percentajeFinished},100`}
+  }
+  styleProgress(){
+    return {"stroke-dasharray":`${this.percentajeProgress},100`}
+  }
 }
