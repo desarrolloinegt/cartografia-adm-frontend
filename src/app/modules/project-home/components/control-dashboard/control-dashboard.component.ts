c03e6105102cd8670f8a7de611212de9bc03e8d9
Lucía Vásquez <bvlucia34@gmail.com>
2023-03-22 19:48:20 -0600

    Convirtiendo imagenes del dashboard a botones para poder dar clic sobre ellas

--- src/app/modules/project-home/components/control-dashboard/control-dashboard.component.ts
+++ src/app/modules/project-home/components/control-dashboard/control-dashboard.component.ts
@@ -6,53 +6,60 @@ import { ProjectHomeService } from '@modules/project-home/services/project-home.
 @Component({
   selector: 'app-control-dashboard',
   templateUrl: './control-dashboard.component.html',
-  styleUrls: ['./control-dashboard.component.scss']
+  styleUrls: ['./control-dashboard.component.scss'],
 })
 export class ControlDashboardComponent {
-  total:number;
-  totalProgres:number;
-  finished:number;
-  progress:number;
-  projectId:number;
-  percentajeFinished:number;
-  percentajeProgress:number;
-  departments:IDepartament[];
-  constructor(private projectHomeService:ProjectHomeService, private dashService:ControlDashboardService){
-    this.total=0;
-    this.finished=0;
-    this.progress=0;
-    this.projectId=-1;
-    this.percentajeFinished=0;
-    this.percentajeProgress=0;
-    this.totalProgres=0;
-    this.departments=[]
+  total: number;
+  totalProgres: number;
+  finished: number;
+  progress: number;
+  projectId: number;
+  percentajeFinished: number;
+  percentajeProgress: number;
+  departments: IDepartament[];
+
+  constructor(
+    private projectHomeService: ProjectHomeService,
+    private dashService: ControlDashboardService) {
+    this.total = 0;
+    this.finished = 0;
+    this.progress = 0;
+    this.projectId = -1;
+    this.percentajeFinished = 0;
+    this.percentajeProgress = 0;
+    this.totalProgres = 0;
+    this.departments = [];
   }
-  ngOnInit(){
-    let project=localStorage.getItem('project')||'';
-    this.projectHomeService.getIdProject(project).subscribe((data)=>{
-      this.projectId=data;
+
+  ngOnInit() {
+    let project = localStorage.getItem('project') || '';
+    this.projectHomeService.getIdProject(project).subscribe((data) => {
+      this.projectId = data;
       this.getData();
-    })
+    });
   }
-  getData(){
-    let data={proyecto_id:this.projectId}
-    this.dashService.getData(data).subscribe((resp)=>{
-      console.log(resp)
-      this.finished=resp.finalizados;
-      this.total=resp.total;
-      this.progress=resp.progreso;
-      this.totalProgres=this.total-this.finished;
-      this.percentajeFinished=(resp.finalizados*100)/this.total;
-      this.percentajeProgress=(resp.progreso*100)/this.total;
-    })
-    this.dashService.getDepartments(data).subscribe((data)=>{
-      this.departments=data;
-    })
+
+  getData() {
+    let data = { proyecto_id: this.projectId };
+    this.dashService.getData(data).subscribe((resp) => {
+      console.log(resp);
+      this.finished = resp.finalizados;
+      this.total = resp.total;
+      this.progress = resp.progreso;
+      this.totalProgres = this.total - this.finished;
+      this.percentajeFinished = (resp.finalizados * 100) / this.total;
+      this.percentajeProgress = (resp.progreso * 100) / this.total;
+    });
+    this.dashService.getDepartments(data).subscribe((data) => {
+      this.departments = data;
+    });
   }
-  styleFinished(){
-    return {"stroke-dasharray":`${this.percentajeFinished},100`}
+
+  styleFinished() {
+    return { 'stroke-dasharray': `${this.percentajeFinished},100` };
   }
-  styleProgress(){
-    return {"stroke-dasharray":`${this.percentajeProgress},100`}
+
+  styleProgress() {
+    return { 'stroke-dasharray': `${this.percentajeProgress},100` };
   }
 }
